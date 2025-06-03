e128d4fc5b446cd7870f51198c380b786523a557
Yaniv_shus <yanivshus@gmail.com>
2025-06-03 18:40:30 +0300

    need to check the code

--- Sm83Emu/src/main.rs
+++ Sm83Emu/src/main.rs
@@ -77,10 +77,16 @@ fn check_logo(barr: &[u8]) -> bool {
     let original_bytes:Vec<u8> = vec![0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D,
                                     0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99,
                                     0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E];
-    if *barr == *original_bytes.as_slice() {
-        return true;
+    return *barr == *original_bytes.as_slice();
+}
+
+fn compute_checksum(barr: &[u8], checksum: u8) -> bool {
+    let mut comp_checksum: u8 = 0;
+    // run from 0x134 to 0x14c to calculate header checksum
+    for i  in GbFileLocations::TitleS as usize..GbFileLocations::HeaderCheckSum as usize  {
+        comp_checksum = comp_checksum - barr[i] -1;
     }
-    false
+    return comp_checksum == checksum;
 }
 
 // for now we will work without MBC -> small games
@@ -92,19 +98,20 @@ struct Mmu {
 enum MmuError {
     NintendoLogoDoesntExists,
     FileReadingErr(io::Error),
+    WrongChecksum
 }
 
 impl fmt::Display for MmuError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::NintendoLogoDoesntExists => write!(f, "The Nintendo logo doesn't exists in the binary, finished execution"),
-            Self::FileReadingErr(err) => write!(f, "io problem {err:?}")
+            Self::FileReadingErr(err) => write!(f, "io problem {err:?}"),
+            Self::WrongChecksum => write!(f, "Problem while calculating checksum"),
         }
     }
 }
 
 impl Mmu {
-    // TODO : Make more self explanatory error message -> use Result<Self, Some Error Enum>
     fn new(file_name: &String) -> Result<Self, MmuError> {
         let gb_file_result= fs::read(file_name);
         let gb_file = match gb_file_result {
@@ -112,9 +119,14 @@ impl Mmu {
             Err(error) => return Err(MmuError::FileReadingErr(error))
         };
         //
-        // slize to check if the nintedo logo match
+        // slice to check if the nintedo logo match
         if !check_logo(&gb_file[GbFileLocations::LogoS as usize..(GbFileLocations::LogoE as usize + 1)]) {
             return Err(MmuError::NintendoLogoDoesntExists);
+        }
+
+        let checksum: u8 = gb_file[GbFileLocations::HeaderCheckSum as usize]; // get checksum by the byte
+        if !compute_checksum(&gb_file[0..(GbFileLocations::HeaderCheckSum as usize + 1)], checksum) {
+            return Err(MmuError::WrongChecksum);
         }
         return Ok(Mmu{
             buffer: gb_file
